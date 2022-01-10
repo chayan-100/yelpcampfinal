2284b38290b66c8d97b46b6f830e3293d23b85e3
Chayan <chayanbittu100@gmail.com>
2022-01-10 16:38:11 +0530

    Adding scripts

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const MongoStore = require('connect-mongo');
 
 const dbUrl = process.env.DB_URL ||'mongodb://localhost:27017/yelp-camp';
 
+
 mongoose.connect(dbUrl, {
    /* useNewUrlParser: true,
     useCreateIndex: true,
